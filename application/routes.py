863297200617ab964ceca63329a719300a47f168
Yongye Tan <yongyetan1209@gmail.com>
2023-01-14 18:27:50 -0500

    add more html, pass data to view and url variables

--- application/routes.py
+++ application/routes.py
@@ -5,16 +5,24 @@ from flask import render_template # use to render html file
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=True)
+    return render_template("index.html", index=True)
 
-@app.route("/courses")
-def courses():
-    return render_template("index.html", login=True)
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term = "Spring 2020"):
+    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, 
+                 {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
+                 {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
+                 {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
+                 {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+    print(courseData[0]["title"])
+    # pass data from python to the html view
+    return render_template("courses.html", courseData=courseData, courses = True, term = term)
 
 @app.route("/register")
 def register():
-    return render_template("index.html", login=True)
+    return render_template("register.html", register=True)
 
 @app.route("/login")
 def login():
-    return render_template("index.html", login=True)
+    return render_template("login.html", login=True)
