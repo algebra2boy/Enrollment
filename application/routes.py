24c994da5d3791cc2e6a70976fa3bd0eb88f876d
Yongye Tan <yongyetan1209@gmail.com>
2023-01-16 18:30:23 -0500

    Creating API route and add Response and json library

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,12 @@
 from application import app
-from flask import render_template, request # use to render html file and pass data 
+from flask import Response, render_template, request,json # use to render html file and pass data 
+
+
+courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, 
+                 {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
+                 {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
+                 {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
+                 {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
 @app.route("/")
 @app.route("/index")
@@ -10,11 +17,6 @@ def index():
 @app.route("/courses/")
 @app.route("/courses/<term>")
 def courses(term = "Spring 2020"):
-    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, 
-                 {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
-                 {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
-                 {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
-                 {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
     # pass data from python to the html view
     return render_template("courses.html", courseData=courseData, course = True, term = term)
 
@@ -26,12 +28,13 @@ def register():
 def login():
     return render_template("login.html", login=True)
 
-@app.route("/enrollment")
+@app.route("/enrollment", methods = ["GET", "POST"])
 def enrollment():
     # print(request.args)
-    courseID = request.args.get('courseID')
-    title = request.args.get('title')
-    term = request.args.get('term')
+    # request.args.get('courseID') -> use this if we only using GET
+    courseID = request.form.get('courseID')
+    title = request.form.get('title')
+    term = request.form.get('term')
 
     course_data = {"id": courseID,
                             "title": title,
@@ -40,3 +43,19 @@ def enrollment():
     return render_template("enrollment.html", 
                             enrollment=True,
                             course_data = course_data)
+
+@app.route("/api/")
+@app.route("/api/<id>")
+def api(id=None):
+    if id == None:
+        json_data = courseData
+    else: 
+        json_data = {"error": "course does not exist"}
+        for course in courseData:
+            if course['courseID'] == id:
+                json_data = course
+                break
+    
+    return Response(json.dumps(json_data),
+                    mimetype="application/json")
+
